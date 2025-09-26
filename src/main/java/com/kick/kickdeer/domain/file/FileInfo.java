25c69e3f43a9366051c23515a39eeeba397b70fc
bangsuji <sjbang@lgupluspartners.co.kr>
2025-09-26 17:34:29 +0900

    신고 이미지 엔티티 생성

--- src/main/java/com/kick/kickdeer/domain/file/FileInfo.java
+++ src/main/java/com/kick/kickdeer/domain/file/FileInfo.java
@@ -1,4 +1,27 @@
 package com.kick.kickdeer.domain.file;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class FileInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long complainId;
+
+    private String fileName;
+
+    private String fileType;
+
+    private String filePath;
+
+    private String fileSize;
+
+
+
 }
