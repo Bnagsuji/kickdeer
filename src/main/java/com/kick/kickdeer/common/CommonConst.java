c697c4bb691bb16eca143cb6f1d9fa4e117eb5c4
bangsuji <sjbang@lgupluspartners.co.kr>
2025-09-26 17:35:07 +0900

    권한별 url 파일 생성

--- src/main/java/com/kick/kickdeer/common/CommonConst.java
+++ src/main/java/com/kick/kickdeer/common/CommonConst.java
@@ -1,4 +1,30 @@
 package com.kick.kickdeer.common;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CommonConst {
+
+    //회원 GET 허용
+    private final String[] PERMIT_ALL_GET_ARRAY = {
+
+    };
+
+    //회원 POST 허용
+    private final String[] PERMIT_ALL_POST_ARRAY = {
+            "/api/complaint/catchDeer"
+    };
+
+
+
+
+    //ADMIN GET 허용
+    private final String[] PERMIT_ADMIN_GET_ARRAY = {
+           "/api/complaint/admin"
+    };
+
+
+
+
+
 }
